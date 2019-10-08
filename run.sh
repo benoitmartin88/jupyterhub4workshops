3d439b8fe425abac7e4843c5b63ecf273a0a98e3
Benoit Martin <benoitmartin88.pro@gmail.com>
2019-10-08 18:03:20 +0200

    docker build and remove image when running run script

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+DOCKER_IMAGE_NAME=jupyterhub_tmp
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd ${SCRIPTPATH}
+
+# build docker image
+docker build -t ${DOCKER_IMAGE_NAME} .
 
 docker run --rm -it \
   -v ${SCRIPTPATH}/data:/srv/jupyterhub \
   -v ${SCRIPTPATH}/user_homes:/home \
   -v ${SCRIPTPATH}/shared_data:/shared_data:ro \
-  -p 8000:8000 --name jupyterhub test_jupyterhub jupyterhub --config=/srv/jupyterhub/jupyterhub_config.py
+  -p 8000:8000 --name jupyterhub ${DOCKER_IMAGE_NAME} jupyterhub --config=/srv/jupyterhub/jupyterhub_config.py
 
+docker rmi ${DOCKER_IMAGE_NAME}
